b691664e899fa430694437a82eca96c1e6e3c709
Shrey Vichare <smashshrey@gmail.com>
2025-04-18 02:07:09 +0530

    fix

---
+++ src/utils/constant.js
@@ -0,0 +1,7 @@
+export const BASE_URL = "http://localhost:8000";
+
+export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
+
+export const TURF_API_END_POINT = `${BASE_URL}/api/v1/turf`;
+
+export const PAYMENT_API_END_POINT = `${BASE_URL}/api/v1/payment`;
